627c23eacea93a1ef2a9812d1473e7113d23e174
unknown <tinph.1992@gmail.com>
2024-01-16 22:23:14 +0700

    fix bug, add vi languages

--- stockwarehouse/settings.py
+++ stockwarehouse/settings.py
@@ -164,7 +164,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ('vi', _('Tiếng Việt')),
+    ('en', _('English')),]
 
 TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
