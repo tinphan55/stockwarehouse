709ad3d63e9809652cf87a125009382f8ff52f1c
khoitq-acl <khoitq@americancodelab.com>
2024-01-18 08:12:57 +0700

    Add UI

--- stockwarehouse/settings.py
+++ stockwarehouse/settings.py
@@ -186,8 +186,13 @@ DATETIME_INPUT_FORMATS = (('%d-%m-%Y %H:%i'),)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static/'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
