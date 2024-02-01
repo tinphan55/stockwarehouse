3378cf1c86800be896de9a3ee6c722c92b973b2c
tinphan55 <tinph.1992@gmail.com>
2024-02-01 21:38:34 +0700

    fix https

--- stockwarehouse/settings.py
+++ stockwarehouse/settings.py
@@ -35,9 +35,7 @@ SECRET_KEY = 'django-insecure-1@%_-7!z=r9nf#$rbge-n10+fs@9x)q8=b2o0qbl&8)n$%#x0x
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 CSRF_TRUSTED_ORIGINS = ['https://ecotrading.com.vn']
-
 
 ALLOWED_HOSTS = ['*']
 
